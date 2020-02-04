abfab076740dd8fb98f3750773be3187b8ed2478
ZheWenYang <1850036635@qq.com>
2020-02-04 11:07:53 +0800

    增加class_03文件夹，第三次课的课程代码。

---
+++ primary_class(1-9)/class_03/class_03_01.py
@@ -0,0 +1,13 @@
+# 绘制一个三角形
+import turtle
+
+turtle.forward(200)
+turtle.left(120)
+
+turtle.forward(200)
+turtle.left(120)
+
+turtle.forward(200)
+turtle.left(120)
+
+turtle.done()
---
+++ primary_class(1-9)/class_03/class_03_02.py
@@ -0,0 +1,16 @@
+# 绘制一个正方形
+import turtle
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+
+turtle.done()
