17ae401c15b6e0c74e87d255cef195735dac449e
ZheWenYang <1850036635@qq.com>
2020-02-04 11:23:40 +0800

    增加class_03文件夹，第三次课的课程讲义。

---
+++ primary_class(1-9)/class_03/class_03_03.py
@@ -0,0 +1,17 @@
+# 使用for循环绘制三角形
+import turtle
+
+for i in range(3):
+    turtle.forward(200)
+    turtle.left(120)
+
+turtle.done()
+
+# 使用for循环绘制正方形
+# import turtle
+#
+# for i in range(4):
+#     turtle.forward(200)
+#     turtle.left(90)
+#
+# turtle.done()
